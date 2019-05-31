f11a9e27a4eff36bec97447af2828322e0eae271
Matt <matthew.wong@outlook.com>
2019-05-31 11:07:02 -0400

    Completed remove row

--- assignment4/pixelate.js
+++ assignment4/pixelate.js
@@ -9,20 +9,23 @@ document.getElementById("add-row").addEventListener("click", function(){
     for(let i = 0; i < number_of_squares; i++){
         let cell = row.insertCell(i)
     }
-
-    
+    current_rows++;
 })
 
 document.getElementById("sub-row").addEventListener("click", function(){
-
+    table.deleteRow(current_rows-1);
+    if(current_rows > 0)
+        current_rows--;
 })
 
 document.getElementById("add-col").addEventListener("click", function(){
-
+    console.log(table.rows)
+    // current_cols++;
 })
 
 document.getElementById("sub-col").addEventListener("click", function(){
-
+    if(current_rows > 0)
+        current_cols--;
 })
 
 document.getElementById("find_all").addEventListener("click", function(){
