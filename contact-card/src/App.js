ff81e026c34b268ee89b386a5db855e209092754
Matt <matthew.wong@outlook.com>
2019-06-03 13:04:11 -0400

    Finished contact card assignment

--- contact-card/src/App.js
+++ contact-card/src/App.js
@@ -4,8 +4,13 @@ import ContactCard from './ContactCard'
 
 function App() {
   return (
+      <div>
+          <ContactCard name="Matthew Wong" mobile_number="111 111 1111" work_number="111 111 1112" email="the@gmail.com" />
+          <ContactCard name="Test User" mobile_number="111 111 1111" work_number="111 115 1112" email="test@gmail.com" />
 
-      <ContactCard />
+          <ContactCard name="Alice" mobile_number="111 111 1113" email="alice@gmail.com" />
+
+      </div>
 
   );
 }
--- contact-card/src/ContactCard.js
+++ contact-card/src/ContactCard.js
@@ -1,6 +1,34 @@
 import React, {Component} from 'react';
 
 class ContactCard extends Component{
+    constructor(props) {
+        console.log(props.name)
+        super(props);
+        this.state = {
+            name: this.props.name,
+            work_number: this.props.work_number,
+            mobile_number: this.props.mobile_number,
+            email:this.props.email
+        };
+        this.updateName = this.updateName.bind(this);
+        this.updateWorkNumber = this.updateWorkNumber.bind(this);
+        this.updateMobileNumber = this.updateMobileNumber.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
+    };
+    updateName(e){
+        console.log(e.target.value)
+        this.setState({name: e.target.value});
+    };
+    updateWorkNumber(e){
+        this.setState({work_number: e.target.value});
+    };
+    updateMobileNumber(e){
+        this.setState({mobile_number: e.target.value});
+    };
+    updateEmail(e){
+        this.setState({email: e.target.value});
+        console.log(this.state.email)
+    };
 
   render(){
       return(
@@ -13,34 +41,37 @@ class ContactCard extends Component{
                             <img className="media-object img-circle profile-img" alt="profile pic" src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" />
                         </div>
                         <div className="media-body">
-                            <h2 className="media-heading">Daniel</h2>
-                            <div className="mnumber">111-111-1111</div>
-                            <div className="wnumber">111-111-1111</div>
-                            <div className="mail"><a href="mailto:daniel@bla.ch">daniel@bla.ch</a> </div>
+                            <h2 className="media-heading">{this.state.name}</h2>
+                            <div className="mnumber">Mobile Number: {this.state.mobile_number}</div>
+                            <div className="wnumber">Work Number: {this.state.work_number}</div>
+                            <div className="mail">Email: <a href={this.state.email}>{this.state.email}</a> </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="col-sm-6">
+                {/*
+                Extra form info. Not necessary. May be useful for future code
                 <form action="#">
-                    <div class="form-group">
-                        <input type="text" className="name form-control" placeholder="Full name" required></input>
+                    <div className="form-group">
+                        <input type="text" className="name form-control" placeholder="Full name" value={this.props.name} onChange={this.updateName} required></input>
                     </div>
 
-                    <div class="form-group">
-                        <input type="tel" className="mobile_number form-control" placeholder="Mobile Number" equired></input>
+                    <div className="form-group">
+                        <input type="tel" className="mobile_number form-control" placeholder="Mobile Number" value={this.props.mobile_number} onChange={this.updateMobileNumber} required></input>
                     </div>
-                    <div class="form-group">
-                        <input type="email" className="email form-control" placeholder="Email Address" equired></input>
+                    <div className="form-group">
+                        <input type="email" className="email form-control" placeholder="Email Address" value={this.props.email} onChange={this.updateEmail} required></input>
                     </div>
-                    <div class="form-group">
-                        <input type="tel" className="work_number form-control" placeholder="Work Number"></input>
+                    <div className="form-group">
+                        <input type="tel" className="work_number form-control" placeholder="Work Number" value={this.props.work_number} onChange={this.updateWorkNumber}></input>
                     </div>
 
-                    <div class="form-group">
+                    <div className="form-group">
                         <input type="button" className="btn btn-primary" id="submit" value="Update Card"></input>
                     </div>
                 </form>
+                */}
             </div>
     	</div>
     </div>
